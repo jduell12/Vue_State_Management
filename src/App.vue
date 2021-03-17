b609f355d7016a037742362843aa80ee31a1f5d6
Jessica <duellje@oregonstate.edu>
2021-03-17 19:41:35 -0400

    added component to add todo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <AddTodo />
     <div class="container">
       <Todos />
     </div>
@@ -8,11 +9,13 @@
 
 <script>
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos, 
+    AddTodo
   }
 }
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="addTodo">
+        <h3>Add Todo</h3>
+        <div class="add">
+            <form>
+                <input type="text" v-model="title" placeholder="Add Todo...">
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo"
+}
+</script>
+
+<style>
+.addTodo{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+}
+
+form{
+    display: flex;
+    flex-direction: column;
+}
+input{
+    margin: 1%;
+}
+
+input[type="text"]{
+    border: 1px solid #41b883;
+}
+
+input[type="submit"]{
+    background: #41b883;
+    color: #fff;
+    border: 1px solid #41b883;
+    cursor: pointer;
+}
+</style>
