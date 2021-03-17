e9feb5e21c575d0fb6ddc2f8d45757a191ad69b4
Jessica <duellje@oregonstate.edu>
2021-03-17 19:47:28 -0400

    added state to add todo

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -13,10 +13,18 @@ const actions = {
     const res = await axios.get("https://jsonplaceholder.typicode.com/todos");
     commit("setTodos", res.data);
   },
+  async addTodo({ commit }, title) {
+    const res = await axios.post("https://jsonplaceholder.typicode.com/todos", {
+      title,
+      completed: false,
+    });
+    commit("newTodo", res.data);
+  },
 };
 
 const mutations = {
   setTodos: (state, todos) => (state.todos = todos),
+  newTodo: (state, todo) => state.todos.unshift(todo),
 };
 
 export default {
